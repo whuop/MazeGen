ea2e1277e90e00d3e9c9e12d175208f947da1d96
Kristian Brodal <kristianbrodal@gmail.com>
2014-10-13 21:50:59 +0200

    Implementing prims algorithm.
    Currently implementing the first version of the randomized prims
    algorithm.

--- PrimsRandomized/src/Main.hx
+++ PrimsRandomized/src/Main.hx
@@ -1,11 +1,12 @@
 import luxe.Input;
-
+import primsrandomized.PrimsMaze;
 
 class Main extends luxe.Game 
 {
     override function ready() 
     {
-
+        var prims = new PrimsMaze( { width : 10, height : 10 } );
+        prims.createMaze();
     } //ready
 
     override function onkeyup( e:KeyEvent ) 
--- PrimsRandomized/src/primsrandomized/PrimsMaze.hx
+++ PrimsRandomized/src/primsrandomized/PrimsMaze.hx
@@ -1,9 +1,57 @@
 package primsrandomized;
+import haxe.ds.Vector;
 
-class PrimzMaze
+typedef PrimzMazeOptions = 
 {
-	public function new()
+	width : Int,
+	height : Int
+};
+
+typedef MazeNode = 
+{
+	up : Bool,
+	down : Bool,
+	left : Bool,
+	right : Bool
+};
+
+class PrimsMaze
+{
+	private var m_openList : Array<MazeNode>;
+
+	private var m_maze : Vector<MazeNode>;
+
+	private var m_options : PrimzMazeOptions;
+
+	public function new(options : PrimzMazeOptions)
 	{
-		
+		m_options = options;
+		m_openList = new Array<MazeNode>();
+		m_maze = new Vector<MazeNode>(options.width * options.height);
+
+		for( i in 0...m_openList.length )
+		{
+			m_maze[i] = { up : false, down : false, left : false, right : false};
+		}
+	}
+
+	public function createMaze()
+	{
+		//	Get a random position to start the maze at.
+		var randomX : Int = Std.random(m_options.width);
+		var randomY : Int = Std.random(m_options.height);
+		makeNeighborsOpen(randomX, randomY);
+
+
+		//	While there are still nodes to visit
+		while ( m_openList.length > 0)
+		{
+
+		}
+	}
+
+	private function makeNeighborsOpen(x : Int, y : Int)
+	{
+
 	}
 }
